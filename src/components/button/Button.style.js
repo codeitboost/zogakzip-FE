2416a79014fdb01a61e2f90b748475171954438e
MijuKim <alwn8918@gmail.com>
2024-08-21 14:33:23 +0900

    Design : 버튼, 탭 컴포넌트 디자인 (#2)

---
+++ src/components/button/Button.style.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Button = styled.div`
+  width: ${(props) => props.width || '400px'};
+  height: ${(props) => props.height || '50px'};
+  border-radius: 6px;
+
+  background-color: #282828;
+  color: #fafafa;
+
+  font-family: Spoqa Han Sans Neo;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 20.03px;
+  letter-spacing: -0.03em;
+  text-align: center;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
