ebdbf97ec4210a259abde825f07d74ccf214fdf2
MijuKim <alwn8918@gmail.com>
2024-08-28 01:27:10 +0900

    Design : 그룹 카드 배치 (#1)

--- src/components/grouplist/groupcard/GroupCard.jsx
+++ src/components/grouplist/groupcard/GroupCard.jsx
@@ -2,33 +2,33 @@ import * as G from './GroupCard.style';
 import card1 from '../../../assets/img/card/card1.png';
 import logo from '../../../assets/img/logo/logo16.svg';
 
-export default function GroupCard() {
+export default function GroupCard({ img, day, isPublic, title, content, badge, memory, like }) {
   return (
     <G.Container>
-      <G.Img src={card1} alt="card1" />
+      {img && <G.Img src={img} alt="card1" />}
       <G.Info>
-        <G.Day>D+100</G.Day>
+        <G.Day>D+{day}</G.Day>
         <div>|</div>
-        <G.Public>공개</G.Public>
+        <G.Public>{isPublic ? '공개' : '비공개'}</G.Public>
       </G.Info>
       <G.Title>
-        <G.Name>에델바이스</G.Name>
-        <G.Content>서로 한 마음으로 응원하고 아끼는 달봉이네 가족입니다.</G.Content>
+        <G.Name>{title}</G.Name>
+        <G.Content>{content}</G.Content>
       </G.Title>
       <G.Count>
         <G.Badge>
           <G.BadgeTitle>획득 배지</G.BadgeTitle>
-          <G.BadgeCount>2</G.BadgeCount>
+          <G.BadgeCount>{badge}</G.BadgeCount>
         </G.Badge>
         <G.Memory>
           <G.MemoryTitle>추억</G.MemoryTitle>
-          <G.MemoryCount>8</G.MemoryCount>
+          <G.MemoryCount>{memory}</G.MemoryCount>
         </G.Memory>
         <G.Like>
           <G.LikeTitle>그룹 공감</G.LikeTitle>
           <G.LikeCount>
             <img src={logo} alt="logo" />
-            <div>1.5K</div>
+            <div>{like}</div>
           </G.LikeCount>
         </G.Like>
       </G.Count>
---
+++ src/pages/GroupList.style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 1560px;
+  max-height: 1400px;
+
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  gap: 20px;
+  align-items: start;
+  overflow: hidden;
+
+  margin-bottom: 60px;
+`;
