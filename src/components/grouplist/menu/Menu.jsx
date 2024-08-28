7ca9002d19f1061d1380bb8fbd6b01c48ef2695c
MijuKim <alwn8918@gmail.com>
2024-08-28 23:36:31 +0900

    Design : 그룹 만들기 버튼 추가 (#1)

--- src/components/grouplist/menu/Menu.jsx
+++ src/components/grouplist/menu/Menu.jsx
@@ -1,3 +1,4 @@
+import Button from '../../common/button/Button';
 import Dropdown from '../../common/dropdown/Dropdown';
 import Search from '../../common/search/Search';
 import Tab from '../../common/tab/Tab';
@@ -6,12 +7,17 @@ import * as M from './Menu.style';
 export default function Menu() {
   return (
     <M.Container>
-      <M.Tab>
-        <Tab text="공개" />
-        <Tab text="비공개" />
-      </M.Tab>
-      <Search placeholder="그룹명을 검색해 주세요" />
-      <Dropdown />
+      <M.Button>
+        <Button text="그룹 만들기" width="200px" height="45px" fontsize="14px" />
+      </M.Button>
+      <M.Menu>
+        <M.Tab>
+          <Tab text="공개" />
+          <Tab text="비공개" />
+        </M.Tab>
+        <Search placeholder="그룹명을 검색해 주세요" />
+        <Dropdown />
+      </M.Menu>
     </M.Container>
   );
 }
--- src/components/grouplist/menu/Menu.style.js
+++ src/components/grouplist/menu/Menu.style.js
@@ -1,8 +1,23 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  position: relative;
+`;
+
+export const Button = styled.div`
+  position: absolute;
+  right: 0;
+  bottom: 105px;
+`;
+
+export const Menu = styled.div`
   display: flex;
-  margin-bottom: 60px;
+  justify-content: space-between;
+  margin: 40px 0px 60px 0px;
+
+  div {
+    margin: 0;
+  }
 `;
 
 export const Tab = styled.div`
