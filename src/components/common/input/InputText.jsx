16a53e888b247ec57a3e39f7a9411b5073afc225
MijuKim <alwn8918@gmail.com>
2024-08-30 05:28:08 +0900

    Feat : input 유효성 검사 기능 추가 (#2)

--- src/components/common/input/InputText.jsx
+++ src/components/common/input/InputText.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
 import * as I from './InputText.style';
 
 export default function InputText({ title, children }) {
+  const [value, setValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
+
+  const handleChange = (event) => {
+    const newValue = event.target.value;
+    setValue(newValue);
+
+    // 유효성 검사
+    const regex = /^[a-zA-Z0-9ㄱ-힣ㄱ-ㅎㅏ-ㅣ!@#$%^_]*$/;
+    setIsValid(regex.test(newValue));
+  };
+
   return (
-    <I.Container>
+    <I.Container isInvalid={!isValid}>
       <I.Title>{title}</I.Title>
-      <I.Input placeholder={children} />
+      <I.Input placeholder={children} value={value} onChange={handleChange} isInvalid={!isValid} />
+      {!isValid && <I.Error>특수문자는 !@#$%^_만 사용하실 수 있습니다.</I.Error>}
     </I.Container>
   );
 }
--- src/components/common/input/InputText.style.js
+++ src/components/common/input/InputText.style.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  margin-bottom: 40px;
+  margin-bottom: ${(props) => (props.isInvalid ? '15px' : '40px')};
 `;
 
 export const Title = styled.div`
@@ -31,4 +31,18 @@ export const Input = styled.input`
   &::placeholder {
     color: #b8b8b8;
   }
+
+  &:focus {
+    border-color: ${(props) => (props.isInvalid ? '#FF5757' : '#282828')};
+    outline: none; /* 기본 아웃라인 제거 */
+  }
+`;
+
+export const Error = styled.div`
+  margin-top: 10px;
+
+  color: #ff5757;
+  font-size: 12px;
+  font-weight: 400;
+  letter-spacing: -0.36px;
 `;
