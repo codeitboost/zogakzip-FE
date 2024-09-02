0a2bbc246d874ab17569f4018f55bce374e326d3
MijuKim <alwn8918@gmail.com>
2024-09-03 02:17:24 +0900

    Design : NoMemoryList (#24)

--- src/components/memorylist/MemoryList.jsx
+++ src/components/memorylist/MemoryList.jsx
@@ -5,6 +5,7 @@ import MemoryCard from './MemoryCard';
 import * as M from './MemoryList.style';
 import Menu from '../grouplist/menu/Menu';
 import More from '../common/more/More';
+import NoMemoryList from './NoMemoryList';
 
 export default function MemoryList() {
   const { memories } = useContext(MemoryContext);
@@ -12,29 +13,35 @@ export default function MemoryList() {
   return (
     <>
       <Menu type="추억" />
-      <M.Container>
-        <Masonry
-          breakpointCols={4} // 열 개수
-          className="my-masonry-grid"
-          columnClassName="my-masonry-grid_column"
-        >
-          {memories.map((item) => (
-            <MemoryCard
-              key={item.id}
-              id={item.id}
-              img={item.img}
-              day={item.day}
-              isPublic={item.isPublic}
-              title={item.title}
-              content={item.content}
-              badge={item.badge}
-              memory={item.memory}
-              like={item.like}
-            />
-          ))}
-        </Masonry>
-      </M.Container>
-      <More />
+      {memories.length === 0 ? (
+        <NoMemoryList />
+      ) : (
+        <>
+          <M.Container>
+            <Masonry
+              breakpointCols={4} // 열 개수
+              className="my-masonry-grid"
+              columnClassName="my-masonry-grid_column"
+            >
+              {memories.map((item) => (
+                <MemoryCard
+                  key={item.id}
+                  id={item.id}
+                  img={item.img}
+                  day={item.day}
+                  isPublic={item.isPublic}
+                  title={item.title}
+                  content={item.content}
+                  badge={item.badge}
+                  memory={item.memory}
+                  like={item.like}
+                />
+              ))}
+            </Masonry>
+          </M.Container>
+          <More />
+        </>
+      )}
     </>
   );
 }
