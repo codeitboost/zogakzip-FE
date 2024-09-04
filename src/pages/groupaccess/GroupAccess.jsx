3a507a76759e02454737d2b1271d3e4bc8fcf1ce
MijuKim <alwn8918@gmail.com>
2024-09-04 10:08:12 +0900

    Fix : 비공개 그룹 접근 페이지 디자인 수정

--- src/pages/groupaccess/GroupAccess.jsx
+++ src/pages/groupaccess/GroupAccess.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import * as S from './GroupAccess.style';
 import Button from '../../components/common/button/Button';
+import InputText from '../../components/common/input/InputText';
 
 export default function GroupAccess() {
   const [password, setPassword] = useState('');
@@ -22,19 +23,13 @@ export default function GroupAccess() {
 
   return (
     <S.Container>
-      <S.Logo src="/path/to/logo.png" alt="로고 이미지" />
       <S.Title>비공개 그룹</S.Title>
       <S.Subtitle>비공개 그룹에 접근하기 위해 권한 확인이 필요합니다</S.Subtitle>
       <S.PasswordContainer>
-        <S.PasswordInput
-          type="password"
-          placeholder={placeholder}
-          value={password}
-          onChange={handlePasswordChange}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-        />
-        <Button text="제출하기" width="100%" height="50px" fontsize="16px" />
+        <InputText title="비밀번호를 입력해 주세요" onChange={handlePasswordChange}>
+          비밀번호를 입력해 주세요
+        </InputText>
+        <Button text="제출하기" fontsize="16px" />
       </S.PasswordContainer>
     </S.Container>
   );
